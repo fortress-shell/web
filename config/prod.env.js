37bbf350d5a4da481f24b8850b1bb63614027cbf
Mikhail Faraponov <mikefaraponov@gmail.com>
2018-04-09 19:39:33 +0300

    Updated

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 const path = require('path');
 
 module.exports = {
   NODE_ENV: '"production"',
-  SESSION: '"http://fortress.sh/v1/sessions"',
-  API_URL: '"http://fortress.sh/"',
+  SESSION: '"https://fortress.sh/v1/sessions"',
+  API_URL: '"https://fortress.sh/"',
   API_TIMEOUT: '"10000"',
   GITHUB_OAUTH: '"https://github.com/login/oauth/authorize?client_id=aa942c520baf3f84f67f&scope=admin:org_hook%20admin:repo_hook%20admin:public_key%20repo%20user"',
   SOCKET_URL: '"wss://fortress.sh"',
   SOCKET_PATH: '"/notifications"',
-}
+};
---
+++ static/index.js
@@ -0,0 +1,1 @@
+window.SESSION_STATE = false;
